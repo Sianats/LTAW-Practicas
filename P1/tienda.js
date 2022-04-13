111ea74e47daf6567ae59af7366994c2add13345
sianats <a.torres.2018@alumnos.urjc.es>
2022-04-13 19:00:13 +0200

    arranque del servidor

--- P1/tienda.js
+++ P1/tienda.js
@@ -1,45 +1,11 @@
+//-- Importar los modulos http, fs y url
 const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
 //-- Definir el puerto a utilizar
-const PUERTO = 8080;
+const PUERTO = 9090;
 
-//-- Crear el servidor
-const server = http.createServer((req, res) => {
-    
-  //-- Indicamos que se ha recibido una petición
-  console.log("Petición recibida!");
+//-- Mensaje de arranque del servidor
+console.log("Iniciando servidor...");
 
-  //-- Cabecera que indica el tipo de datos del
-  //-- cuerpo de la respuesta: Texto plano
-  res.setHeader('Content-Type', 'text/plain');
-
-  //-- Mensaje del cuerpo
-  res.write("Happy server!!\n");
-
-  //-- Terminar la respuesta y enviarla
-  res.end();
-});
-
-const fs = require('fs');
-
-//-- Fichero a leer
-const FICHERO = 'fichero.txt';
-
-fs.readFile(FICHERO, 'utf8', (err, data) => {
-
-    if (err) {  //-- Ha ocurrido algun error
-      console.log("Error!!")
-      console.log(err.message);
-    } 
-    else {  //-- Lectura normal
-        console.log("Lectura completada...")
-        console.log("Contenido del fichero: \n")
-        console.log(data);
-    }
-})
-
-
-//-- Activar el servidor: ¡Que empiece la fiesta!
-server.listen(PUERTO);
-
-console.log("Happy server activado!. Escuchando en puerto: " + PUERTO);
