94e2392f27d203f71f7e024d4d1e698c0e72c0c2
sianats <a.torres.2018@alumnos.urjc.es>
2022-04-26 15:20:49 +0200

    no he sacado nada en claro

--- P1/tienda.js
+++ P1/tienda.js
@@ -4,12 +4,20 @@ const url = require('url');
 
 const PUERTO = 9090;
 
-const server = http.createServer((req, res)=>{
-    console.log("Petición recibida!");
+let
 
-
+const server = http.createServer((req, res) => {
+    statusCode = 200;
+    contentType =  {'Content-Type': 'text/html'};
+    fs.readFile("tienda.html", (err, data) => {
+       if(err){
+         throw err;
+       }
+       res.writeHead(200, {'Content-Type': 'text/html'});
+       res.write(data);
+       res.end();
+    });
 });
-
 server.listen(PUERTO);
 
 console.log("Escuchando en puerto: " + PUERTO);
