1f21adc999d0787ea12c8a4c139e0af775d0bcf8
sianats <a.torres.2018@alumnos.urjc.es>
2022-04-28 20:34:51 +0200

    Me ha llevado todo el día pero ya tengo el servidor de node :)

--- P1/tienda.js
+++ P1/tienda.js
@@ -1,23 +1,33 @@
 const http = require('http');
 const fs = require('fs');
-const url = require('url');
 
 const PUERTO = 9090;
 
-let
-
 const server = http.createServer((req, res) => {
-    statusCode = 200;
-    contentType =  {'Content-Type': 'text/html'};
-    fs.readFile("tienda.html", (err, data) => {
-       if(err){
-         throw err;
-       }
-       res.writeHead(200, {'Content-Type': 'text/html'});
-       res.write(data);
-       res.end();
-    });
+  let myURL = new URL (req.url, 'http://' + req.headers['host']);
+
+  let page = "";
+
+   if (myURL.pathname != "/"){
+       page += "."+ myURL.pathname
+     } else{
+         page += "tienda.html"
+     }
+
+  fs.readFile(page, function(err, data) {
+
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return fs.createReadStream('error.html').pipe(res)
+    } 
+
+    res.write(data);
+    res.end();
+  });
+
+
 });
+
 server.listen(PUERTO);
 
 console.log("Escuchando en puerto: " + PUERTO);
