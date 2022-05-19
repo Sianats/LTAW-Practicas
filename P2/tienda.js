94c3e2bc9a83f46b7d8fbbee64415ff12e879519
sianats <a.torres.2018@alumnos.urjc.es>
2022-05-19 18:51:31 +0200

    practica 3

--- P2/tienda.js
+++ P2/tienda.js
@@ -1,104 +1,115 @@
-//-- Servidor JSON
-console.log("Ejecutando JS....")
-
 const http = require('http');
 const fs = require('fs');
+
 const PUERTO = 9090;
 
-//-- Cargar la Página de error
-const ERROR = fs.readFileSync('error.html');
-
-//-- Cargar pagina web principal
-const MAIN = fs.readFileSync('busqueda.html','utf-8');
-
-//-- Leer fichero JSON con los productos 
 const PRODUCTOS_JSON = fs.readFileSync('productos.json');
-
-//-- Obtener el array de productos
 let productos = JSON.parse(PRODUCTOS_JSON);
 
-//-- SERVIDOR: Bucle principal de atención a clientes
 const server = http.createServer((req, res) => {
+  let myURL = new URL (req.url, 'http://' + req.headers['host']);
 
-    //-- Construir el objeto url con la url de la solicitud
-    const myURL = new URL(req.url, 'http://' + req.headers['host']);  
+  let page = "";
 
-    //-- Variables para el mensaje de respuesta
-    let content_type = "text/html";
-    let content = "";
-  
-    //-- Leer recurso y eliminar la / inicial
-    let recurso = myURL.pathname;
-    recurso = recurso.substr(1); 
+   if (myURL.pathname != "/"){
+       page += "."+ myURL.pathname
+     } else{
+         page += "tienda.html"
+     }
 
-    switch (recurso) {
-        case '':
-            console.log("Main page");
-            content = MAIN;
-            break;
+  fs.readFile(page, function(err, data) {
 
-        case 'productos':
-            console.log("Peticion de Productos!")
-            content_type = "application/json";
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return fs.createReadStream('error.html').pipe(res)
+    } 
 
-            //-- Leer los parámetros
-            let param1 = myURL.searchParams.get('param1');
-
-            param1 = param1.toUpperCase();
-
-            console.log("  Param: " +  param1);
-
-            let result = [];
-
-            for (let prod of productos) {
-
-                //-- Pasar a mayúsculas
-                prodU = prod.toUpperCase();
-
-                //-- Si el producto comienza por lo indicado en el parametro
-                //-- meter este producto en el array de resultados
-                if (prodU.startsWith(param1)) {
-                    result.push(prod);
-                }
-                
-            }
-            console.log(result);
-            content = JSON.stringify(result);
-            break;
-
-        case 'cliente.js':
-            //-- Leer fichero javascript
-            console.log("recurso: " + recurso);
-            fs.readFile(recurso, 'utf-8', (err,data) => {
-                if (err) {
-                    console.log("Error: " + err)
-                    return;
-                } else {
-                  res.setHeader('Content-Type', 'application/javascript');
-                  res.write(data);
-                  res.end();
-                }
-            });
-            
-            return;
-            break;
-
-            //-- Si no es ninguna de las anteriores devolver mensaje de error
-        default:
-            res.setHeader('Content-Type','text/html');
-            res.statusCode = 404;
-            res.write(ERROR);
-            res.end();
-            return;
-    }
-  
-    //-- Generar respuesta
-    res.setHeader('Content-Type', content_type);
-    res.write(content);
-    res.end()
-  
+    res.write(data);
+    res.end();
   });
+
+
+});
+
+server.listen(PUERTO);
+
+console.log("Escuchando en puerto: " + PUERTO);
+
+ 
+//     //-- Variables para el mensaje de respuesta
+//     let content_type = "text/html";
+//     let content = "";
   
-  server.listen(PUERTO);
-  console.log("Escuchando en puerto: " + PUERTO);
+//     //-- Leer recurso y eliminar la / inicial
+//     let recurso = myURL.pathname;
+//     recurso = recurso.substr(1); 
+
+//     switch (recurso) {
+//         case '':
+//             console.log("Main page");
+//             content = MAIN;
+//             break;
+
+//         case 'productos':
+//             console.log("Peticion de Productos!")
+//             content_type = "application/json";
+
+//             //-- Leer los parámetros
+//             let param1 = myURL.searchParams.get('param1');
+
+//             param1 = param1.toUpperCase();
+
+//             console.log("  Param: " +  param1);
+
+//             let result = [];
+
+//             for (let prod of productos) {
+
+//                 //-- Pasar a mayúsculas
+//                 prodU = prod.toUpperCase();
+
+//                 //-- Si el producto comienza por lo indicado en el parametro
+//                 //-- meter este producto en el array de resultados
+//                 if (prodU.startsWith(param1)) {
+//                     result.push(prod);
+//                 }
+                
+//             }
+//             console.log(result);
+//             content = JSON.stringify(result);
+//             break;
+
+//         case 'cliente.js':
+//             //-- Leer fichero javascript
+//             console.log("recurso: " + recurso);
+//             fs.readFile(recurso, 'utf-8', (err,data) => {
+//                 if (err) {
+//                     console.log("Error: " + err)
+//                     return;
+//                 } else {
+//                   res.setHeader('Content-Type', 'application/javascript');
+//                   res.write(data);
+//                   res.end();
+//                 }
+//             });
+            
+//             return;
+//             break;
+
+//             //-- Si no es ninguna de las anteriores devolver mensaje de error
+//         default:
+//             res.setHeader('Content-Type','text/html');
+//             res.statusCode = 404;
+//             res.write(ERROR);
+//             res.end();
+//             return;
+//     }
+  
+//     //-- Generar respuesta
+//     res.setHeader('Content-Type', content_type);
+//     res.write(content);
+//     res.end()
+  
+//   });
+  
   
