474a22e1f20ca1db0e9414ba4e23da0a47db4e2e
Sianats <79252137+Sianats@users.noreply.github.com>
2022-05-20 20:51:59 +0200

    ya tengo casi todo :)

--- P3/server.js
+++ P3/server.js
@@ -32,20 +32,31 @@ app.use(express.static('public'));
 //------------------- GESTION SOCKETS IO
 //-- Evento: Nueva conexion recibida
 io.on('connect', (socket) => {
+  var num = 0;
   console.log('** NUEVA CONEXIÓN **'.yellow);
   socket.write('¡Bienvenido al chat de ISAM!');
+  num += 1;
   //-- Evento de desconexión
   socket.on('disconnect', function(){
     console.log('** CONEXIÓN TERMINADA **'.yellow);
     io.send('Un usuario ha abandonado el chat');
+    num -= 1;
   });  
 
   //-- Mensaje recibido: Reenviarlo a todos los clientes conectados
-  socket.on("message", (msg)=> {
-    if (msg == '/help'){
-      socket.write('te ayudo');
-    }else if(msg == '/hello'){
-      socket.write('hola');
+  socket.on("message", (msg, num)=> {
+    if (msg.includes('/help') && !msg.includes(' se ha unido</h5>')){
+      socket.write('Los comandos disponibles son: <br> <strong>/help:</strong> Este mismo :) <br> <strong>/list:</strong> Devolverá el número de usuarios conectados <br> <strong>/hello:</strong> El servidor nos devolverá el saludo <br> <strong>/date:</strong> Nos devolverá la fecha');
+    }else if(msg.includes('/hello') && !msg.includes(' se ha unido</h5>')){
+      socket.write('¡Hola! Mucho ánimo con la carrera :)');
+    }else if(msg.includes('/list') && !msg.includes(' se ha unido</h5>')){
+socket.write(num);
+    }else if(msg.includes('/date') && !msg.includes(' se ha unido</h5>')){
+      const tiempoTranscurrido = Date.now();
+      const hoy = new Date(tiempoTranscurrido);
+      socket.write('Hoy es: ' + hoy.toLocaleString('es-ES'));
+    }else if(msg.includes('/' + '') && !msg.includes(' se ha unido</h5>') && !msg.includes('/date') && !msg.includes('/list') && !msg.includes('/help') && !msg.includes('/hello') ){
+      socket.write('Lo siento pero no es un comando válido :(');
     }else{
         console.log("Mensaje Recibido!: " + msg.blue);
         //-- Reenviarlo a todos los clientes conectados
